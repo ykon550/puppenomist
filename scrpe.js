bfce5073365844d64b3b77376a5c6b145a75b57b
yzwhy <28885823+yzwhy@users.noreply.github.com>
2018-07-30 23:58:51 -0700

    1st commit with working features

--- scrpe.js
+++ scrpe.js
@@ -1,15 +1,13 @@
 const ppt = require('puppeteer');
 require('dotenv').config();
-
 const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
+const CRAWL_DELAY=5000;
+const fs = require('fs');
 
 (async () => {
-    console.log('hoge');
-    const browser = await ppt.launch({ headless: false });
-    console.log('hoge');
+    const browser = await ppt.launch();
     const page = await browser.newPage();
-    await page.setViewport({ width: 1200, height: 1000 });
-    console.log('hoge');
+    await page.setViewport({ width: 1400, height: 1000 });
 
     //Login
     await page.goto('https://www.economist.com', { waitUntil: 'networkidle2' });
@@ -36,41 +34,50 @@ const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
         const main = document.querySelector('main');
         const as = main.querySelectorAll('a');
         let _links = [];
-        for(const a of as){
+        for (const a of as) {
             const href = a.getAttribute('href');
-            if(href.includes('/news/')){
+            if (href.includes('/news/')) {
                 _links.push(a.getAttribute('href'))
             }
         }
         return _links;
     })
-    console.log(links);
-
-    // const nodes = await page.$x('//main//ul//a');
-    // let links = [];
-    // for( const node of nodes){
-    //     links.push(node.getAttribute('href'));
-    // }
-    // console.log(links);
+    //console.log(links);
 
     // extract article
-    await page.goto('https://www.economist.com/the-world-this-week/2018/07/26/politics-this-week', {waitUntil:'networkidle2'});
-    const article = await page.evaluate(() => {
-        const articleElem = document.querySelector('article');
-        const ps = articleElem.querySelectorAll('p');
-        let _text = [];
-        for(const p of ps){
-            _text.push(p.textContent);
-        }
-        return _text;
-    })
-    console.log(article);
+    const numArticles = links.length;
+    for (const [idx, link] of links.entries()) {
+        await sleep(CRAWL_DELAY);
+        let article = {
+            title: '',
+            text: [],
+            url: '',
+            date: '',
+            section: '',
+            filename: ''
+        };
 
-    // const hoge = await page.$x('//article//p/text()');
-    // console.log(hoge);
+        await page.goto('https://www.economist.com' + link, { waitUntil: 'networkidle2' });
+        // expected URL example, https://www.economist.com/middle-east-and-africa/2018/07/26/zimbabwes-opposition-is-gaining-ground-ahead-of-upcoming-elections
+        article.url = await page.url();
+        article.title = article.url.split('/').slice(-4).join('_');
+        article.section = article.url.split('/').slice(3,4).join();
+        article.date = article.url.split('/').slice(4,7).join('');
+        article.filename = article.title + '.json';
 
-
-
-
+        article.text = await page.evaluate(() => {
+            const articleElem = document.querySelector('article');
+            const ps = articleElem.querySelectorAll('p');
+            let _text = [];
+            for (const p of ps) {
+                _text.push(p.textContent);
+            }
+            return _text;
+        })
+        fs.writeFile('./articles/' + article.filename, JSON.stringify(article, null, '\t'), (err) => {
+            if(err) console.log("error: " + err);
+            console.log(idx+1 + '/' + numArticles + ': ' + article.filename + " saved!");
+        })
+    }
 })();
 
