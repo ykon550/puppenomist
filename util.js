cd883a0e3b013262845b3bde84671940f733562a
yzwhy <28885823+yzwhy@users.noreply.github.com>
2018-08-02 21:42:02 -0700

    add Integer cast

--- util.js
+++ util.js
@@ -3,6 +3,8 @@ const CRAWL_DELAY = process.env.CRAWL_DELAY || 5500;
 
 const sleep = (msec = CRAWL_DELAY) => new Promise(resolve => setTimeout(resolve, msec));
 const randomSleep = (min = CRAWL_DELAY, max = 7000) => {
+    min = parseInt(min);
+    max = parseInt(max);
     const wait = Math.random() * (max - min) + min;
     return new Promise(resolve => setTimeout(resolve, wait))
 };
