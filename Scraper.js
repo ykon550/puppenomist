4c1a59cf5d890d29321332cdb944e1f4315c67d6
yzwhy <28885823+yzwhy@users.noreply.github.com>
2018-08-05 14:41:12 -0700

    implmented retry feature for page loading

--- Scraper.js
+++ Scraper.js
@@ -11,6 +11,25 @@ class Scraper {
         return this;
     }
 
+    // wrapper for Puppeteer.page.goto()
+    // retry if failed.
+    async goto(link) {
+        const RETRY_MAX = 3;
+        for (let cnt = 1; cnt <= RETRY_MAX; cnt++) {
+            try{
+                await this.page.goto(link, { timeout: 30000, waitUntil: 'networkidle2' });
+                break;
+            }catch(err){
+                console.log("page loading failed for " + cnt + " times");
+                if(cnt == RETRY_MAX){
+                    console.log("retried " + RETRY_MAX + " times but still failed, will be skipped " + link);
+                    throw Error("Max retried");
+                }
+                console.log("retrying, ");
+            }
+        }
+    }
+
     async scrapeArticle(link) {
         let article = {
             title: '',
@@ -21,7 +40,7 @@ class Scraper {
             filename: ''
         };
         await util.randomSleep();
-        await this.page.goto(link, { timeout: 100000, waitUntil: 'networkidle2' });
+        await this.goto(link);
         // expected URL example, https://www.economist.com/middle-east-and-africa/2018/07/26/zimbabwes-opposition-is-gaining-ground-ahead-of-upcoming-elections
         article.url = await this.page.url();
         article.title = article.url.split('/').slice(-4).join('_');
@@ -29,7 +48,7 @@ class Scraper {
         article.date = article.url.split('/').slice(4, 7).join('');
         article.dir = article.url.split('/').slice(4, 6).join('');
         article.filename = article.title + '.json';
-    
+
         article.text = await this.page.evaluate(() => {
             let _text = [];
             try {
@@ -43,18 +62,23 @@ class Scraper {
                 console.log("skipped:" + article.url);
             }
             return _text;
-        }).catch((err) => console.log(err));
+        }).catch((err) => {
+            throw Error(err.message);
+        });
         return article;
     }
 
     async scrape() {
         const numArticles = this.links.length;
         for (const [idx, link] of this.links.entries()) {
-            const article = await this.scrapeArticle(link)
-                .catch((err)=>{
-                    console.log(err);
-                    console.log("skipped " + link);
-                });
+            let article = {};
+            try {
+                article = await this.scrapeArticle(link);
+            } catch (err) {
+                console.log(err);
+                console.log("skipped " + link);
+                continue;
+            }
             const dir = './articles/' + article.dir + '/';
             if (!fs.existsSync(dir)) {
                 fs.mkdirSync(dir);
