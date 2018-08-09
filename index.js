1deb5f95406ee6afd2ca76d9d312683d5f663bcd
yzwhy <28885823+yzwhy@users.noreply.github.com>
2018-08-08 22:13:12 -0700

    refactor for npm package

---
+++ index.js
@@ -0,0 +1,47 @@
+const ppt = require('puppeteer');
+require('dotenv').config();
+const login = require('./lib/login');
+const extractArticleLinks = require('./lib/extractArticleLinks');
+const extractIssueLinks = require('./lib/extractIssueLinks');
+const Scraper = require('./lib/Scraper');
+
+let issueLinks = require('./ref/url2017_2.json');
+
+const puppenomist = async () => {
+    const browser = await ppt.launch();
+    //const browser = await ppt.launch({ headless: false });
+    let page = await browser.newPage();
+
+    //disable loading images
+    await page.setRequestInterception(true);
+    page.on('request', request => {
+      if (request.resourceType() === 'image')
+        request.abort();
+      else
+        request.continue();
+    });
+    await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36');
+    await page.setViewport({ width: 1200, height: 1000 });
+
+    await login(page);
+    issueLinks = await extractIssueLinks(page, '2018');
+
+    for (link of issueLinks){
+        const articleLinks = await extractArticleLinks(page, link)
+            .catch((err) => {
+                console.log(err);
+            });
+        console.log("started " + link);
+
+        const scraper = new Scraper(page, articleLinks);
+        await scraper.scrape();
+    }
+
+    await browser.close();
+};
+
+(async()=>{
+    await puppenomist();
+})();
+
+module.exports = puppenomist;
